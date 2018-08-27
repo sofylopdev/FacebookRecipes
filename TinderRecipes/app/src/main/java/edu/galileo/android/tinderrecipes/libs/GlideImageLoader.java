a7ccf34f05d5f812a30e13d86396754c7f2af7ce
sofylopdev <sofylopdev@gmail.com>
2018-08-27 12:34:02 +0100

    feat: Implemented libs

---
+++ TinderRecipes/app/src/main/java/edu/galileo/android/tinderrecipes/libs/GlideImageLoader.java
@@ -0,0 +1,45 @@
+package edu.galileo.android.tinderrecipes.libs;
+
+import android.widget.ImageView;
+
+import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestListener;
+
+import edu.galileo.android.tinderrecipes.libs.base.ImageLoader;
+
+public class GlideImageLoader implements ImageLoader {
+
+    private RequestManager glideRequestManager;
+    private RequestListener onFinishedLoadingListener;
+
+    public GlideImageLoader(RequestManager requestManager) {//Fragment fragment
+        this.glideRequestManager = requestManager;//Glide.with(fragment)
+    }
+
+    @Override
+    public void load(ImageView imageView, String URL) {
+        if(onFinishedLoadingListener != null) {
+            glideRequestManager
+                    .load(URL)
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .centerCrop()
+                   .listener(onFinishedLoadingListener)
+                    .into(imageView);
+        }else{
+            glideRequestManager
+                    .load(URL)
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .centerCrop()
+                    //.override(600, 400)
+                    .into(imageView);
+        }
+    }
+
+    @Override
+    public void setOnFinishedImageLoadingListener(Object listener) {
+        if (listener instanceof RequestListener) {
+            this.onFinishedLoadingListener = (RequestListener) listener;
+        }
+    }
+}
---
+++ TinderRecipes/app/src/main/java/edu/galileo/android/tinderrecipes/libs/di/LibsModule.java
@@ -0,0 +1,58 @@
+package edu.galileo.android.tinderrecipes.libs.di;
+
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestManager;
+
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import edu.galileo.android.tinderrecipes.libs.GlideImageLoader;
+import edu.galileo.android.tinderrecipes.libs.GreenRobotEventBus;
+import edu.galileo.android.tinderrecipes.libs.base.EventBus;
+import edu.galileo.android.tinderrecipes.libs.base.ImageLoader;
+
+@Module
+public class LibsModule {
+    private AppCompatActivity activity;
+
+    public LibsModule(AppCompatActivity activity) {
+        this.activity = activity;
+    }
+
+    @Provides
+    @Singleton
+    EventBus providesEventBus(org.greenrobot.eventbus.EventBus eventBus){
+        return new GreenRobotEventBus(eventBus);
+    }
+
+    @Provides
+    @Singleton
+    org.greenrobot.eventbus.EventBus providesLibraryEventBus(){
+        return org.greenrobot.eventbus.EventBus.getDefault();
+    }
+
+
+    @Provides
+    @Singleton
+    ImageLoader providesImageLoader(RequestManager requestManager){
+        return new GlideImageLoader(requestManager);
+    }
+
+    @Provides
+    @Singleton
+    RequestManager providesRequestManager(Fragment fragment){
+        return Glide.with(fragment);
+    }
+
+    @Provides
+    @Singleton
+    AppCompatActivity providesActivity(){
+        return this.activity;
+    }
+}

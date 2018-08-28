078c22bcdf8a3d4f807ce97916013fcefe3b4ed6
sofylopdev <sofylopdev@gmail.com>
2018-08-28 19:18:44 +0100

    feat: implemented recipeListActivity

--- TinderRecipes/app/src/main/java/edu/galileo/android/tinderrecipes/recipelist/ui/RecipeListActivity.java
+++ TinderRecipes/app/src/main/java/edu/galileo/android/tinderrecipes/recipelist/ui/RecipeListActivity.java
@@ -1,15 +1,128 @@
 package edu.galileo.android.tinderrecipes.recipelist.ui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import edu.galileo.android.tinderrecipes.FacebookRecipesApp;
 import edu.galileo.android.tinderrecipes.R;
+import edu.galileo.android.tinderrecipes.entities.Recipe;
+import edu.galileo.android.tinderrecipes.recipelist.RecipeListPresenter;
+import edu.galileo.android.tinderrecipes.recipelist.ui.adapters.OnItemClickListener;
+import edu.galileo.android.tinderrecipes.recipelist.ui.adapters.RecipesAdapter;
+import edu.galileo.android.tinderrecipes.recipemain.ui.RecipeMainActivity;
 
-public class RecipeListActivity extends AppCompatActivity {
+public class RecipeListActivity extends AppCompatActivity implements RecipeListView, OnItemClickListener {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
+
+    RecipesAdapter adapter;
+    RecipeListPresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_list);
+        ButterKnife.bind(this);
+        setupToolbar();
+        setupInjection();
+        setupRecycler();
+        presenter.onCreate();
+        presenter.getRecipes();
+    }
+
+    @Override
+    protected void onDestroy() {
+        presenter.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_recipes_list, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_main) {
+            navigateToMainScreen();
+        } else if (id == R.id.action_logout) {
+            logout();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        FacebookRecipesApp app = (FacebookRecipesApp) getApplication();
+        app.logout();
+    }
+
+    private void navigateToMainScreen() {
+        startActivity(new Intent(this, RecipeMainActivity.class));
+    }
+
+    private void setupRecycler() {
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void setupInjection() {
+
+    }
+
+    private void setupToolbar() {
+        setSupportActionBar(toolbar);
+    }
+
+    @OnClick(R.id.toolbar)
+    public void onToolbarClick() {
+        recyclerView.smoothScrollToPosition(0);
+    }
+
+    @Override
+    public void setRecipeList(List<Recipe> data) {
+        adapter.setRecipes(data);
+    }
+
+    @Override
+    public void recipeUpdated() {
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void recipeDeleted(Recipe recipe) {
+        adapter.removeRecipe(recipe);
+    }
+
+    @Override
+    public void onFavClick(Recipe recipe) {
+        presenter.toggleFavorite(recipe);
+    }
+
+    @Override
+    public void onItemClick(Recipe recipe) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(recipe.getSourceUrl()));
+        startActivity(intent);
+    }
+
+    @Override
+    public void onDeleteClick(Recipe recipe) {
+        presenter.removeRecipe(recipe);
     }
 }
